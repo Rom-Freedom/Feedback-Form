cd10cc1d0b07b86ce7760103b1a79eeb71b1541c
Rom-Freedom <romeo.dark@mail.ru>
2024-02-03 12:58:08 +0300

    process a form-check: recomendations, stars, upload files

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,31 @@ const review = reactive({
   author: "",
   stars: null,
   text: "",
-  photos: [],
+  photo: null,
   isRecomended: true,
 });
 
-const stars = [1, 2, 3, 4, 5]
+const previewFilePath = computed(() => {
+  if (review.photo) {
+    return URL.createObjectURL(review.photo)
+  }
+  return '#'
+})
+
+const stars = [1, 2, 3, 4, 5];
 
 const submit = () => {
-  console.log('submit!')
+  console.log("submit!");
+};
+
+const uploadFile = (e) => {
+  const [file] = e.target.files;
+  review.photo = file;
 }
 </script>
 
 <template>
-  <form 
-    @submit.prevent.stop="submit"
-    class="container pt-5 pb-5"
-  >
+  <form @submit.prevent.stop="submit" class="container pt-5 pb-5">
     <input
       type="text"
       v-model="review.author"
@@ -36,41 +45,57 @@ const submit = () => {
     ></textarea>
 
     <h4>Оценка</h4>
-    <div
-      v-for="star in stars"
-      :key="star"
-      class="form-check mb-3"
-    >
-      <input class="form-check-input" type="checkbox" value="" :id="`star${ star }`">
-      <label class="form-check-label" :for="`star${ star }`">
+    <div v-for="star in stars" :key="star" class="form-check mb-3">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        v-model="review.stars"
+        :true-value="star"
+        :false-value="null"
+        :id="`star${star}`"
+      />
+      <label class="form-check-label" :for="`star${star}`">
         {{ star }}
       </label>
     </div>
 
     <div class="mb-3">
       <label class="form-label">Фото</label>
-      <input class="form-control" type="file" />
+      <input
+        class="form-control"
+        type="file" 
+        @change="uploadFile"
+      />
+
+      <img :src="previewFilePath" alt="preview img" class="w-100 mt-2">
     </div>
 
     <div class="form-check">
-      <input class="form-check-input" type="radio"  id="adv1">
-      <label class="form-check-label" for="adv1">
-        Не советую
-      </label>
+      <input
+        class="form-check-input"
+        type="radio"
+        id="adv1"
+        v-model="review.isRecomended"
+        :value="false"
+      />
+      <label class="form-check-label" for="adv1"> Не советую </label>
     </div>
 
-      <!-- Form Check -->
-      <div class="form-check mb-3">
-        <input class="form-check-input" type="radio"  id="adv2" checked>
-        <label class="form-check-label" for="adv2">
-          Советую
-        </label>
-      </div>
-      <!-- Form Check -->
+    <!-- Form Check -->
+    <div class="form-check mb-3">
+      <input
+        class="form-check-input"
+        type="radio"
+        id="adv2"
+        v-model="review.isRecomended"
+        :value="true"
+      />
+      <label class="form-check-label" for="adv2"> Советую </label>
+    </div>
+    <!-- Form Check -->
 
     <button class="btn btn-primary">Отправить</button>
   </form>
 </template>
 
-<style>
-</style>
+<style></style>
